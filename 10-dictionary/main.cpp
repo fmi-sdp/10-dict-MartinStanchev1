9386d037c8bed19a5005de36e7e2e5d3ec812e79
MartinStanchev1 <146950292+MartinStanchev1@users.noreply.github.com>
2025-01-13 16:51:59 +0200

    Add files via upload

--- 10-dictionary/main.cpp
+++ 10-dictionary/main.cpp
@@ -1,148 +1,182 @@
-#include <unordered_map>
-#include <string>
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
 
+using std::string;
+using std::unordered_map;
+using std::unordered_set;
+using std::vector;
+using std::pair;
+/*
+class PublicTransportSystem
+{
+	struct BusStop;
+
+	struct BusLine
+	{
+		int number;
+		vector<BusStop*> path;
+		vector<vector<int>> courses;
+	};
+
+	struct BusStop
+	{
+		int number;
+		vector<BusLine*> lines;
+	};
+};
+*/
 //1 zad
-std::unordered_map<std::string, unsigned> countWords(const std::vector<std::string>& words)
+vector<pair<int, int>> findAllPairs(const vector<int>& numbers, int X)
 {
-	std::unordered_map<std::string, unsigned> table;
-	for (const std::string& word : words)
-		table[word]++;
-	return table;
-}
-void printWordCount(const std::unordered_map<std::string, unsigned>& words)
-{
-	for (const std::pair<std::string, unsigned>& el : words)
-		std::cout << el.first << ": " << el.second << std::endl;
+	unordered_map<int, vector<int>> map;
+	vector<pair<int, int>> result;
+	for (int i = 0; i < numbers.size(); i++)
+	{
+		if (map.count(numbers[i]) == 0)
+		{
+			map.insert({ numbers[i], {i} });
+		}
+		else
+		{
+			map[numbers[i]].push_back(i);
+		}
+	}
+
+	for (size_t i = 0; i < numbers.size(); i++)
+	{
+		if (map.count(X - numbers[i]))
+		{
+			for (size_t j = 0; j < map[X - numbers[i]].size(); j++)
+			{
+				result.push_back({ i, map[X - numbers[i]][j]});
+			}
+		}
+	}
+	return result;
 }
 
 //2 zad
-std::unordered_map<std::string, std::vector<std::string>> categorize(const std::vector<std::pair<std::string, std::string>>& objects)
+bool containsAll(const string& word, const unordered_set<char>& set)
 {
-	std::unordered_map<std::string, std::vector<std::string>> table;
-	for (const std::pair<std::string, std::string>& el : objects)
-		table[el.second].push_back(el.first);
-	return table;
-}
-
-void printCategories(const std::unordered_map<std::string, std::vector<std::string>>& table)
-{
-	for (const std::pair<std::string, std::vector<std::string>>& el : table)
+	for (size_t i = 0; i < word.size(); i++)
 	{
-		std::cout << el.first << ": {";
-		for (const std::string& obj : el.second)
-			std::cout << obj << " ";
-		std::cout << "}" << std::endl;
-	}
-
-}
-
-using std::string;
-using std::vector;
-using std::unordered_map;
-using std::pair;
-//3 zad
-unordered_map<char, char> insertRules(const vector<pair<char, char>>& rules)
-{
-	unordered_map<char, char> systematicRules;
-	for (const pair<char, char>& rule : rules)
-		systematicRules[rule.first] = rule.second;
-	return systematicRules;
-}
-
-void changeText(const unordered_map<char, char>& rules, string& text)
-{
-	for (char& symbol : text)
-	{
-		unordered_map<char, char>::const_iterator it = rules.find(symbol);
-		if (it != rules.end())
-			symbol = (*it).second;
-	}
-}
-
-//5 zad
-void printPair(pair<int, int> pair)
-{
-	std::cout << "(" << pair.first << ", " << pair.second << ") ";
-}
-void findAllSymmetricalPairs(const vector<pair<int, int>>& pairs)
-{
-	std::unordered_map<int, int> map;
-	for (size_t i = 0; i < pairs.size(); i++)
-	{
-		int first = pairs[i].first;
-		int second = pairs[i].second;
-		if (map.find(second) != map.end() && map[second] == first)
-		{
-
-			printPair(pair<int, int>(second, first));
-			printPair(pairs[i]);
-			std::cout << std::endl;
-		}
-		map[first] = second;
-	}
-}
-
-//6 zad
-bool areIzomorph(const string& word1, const string& word2)
-{
-	unordered_map<char, char> map;
-	if (word1.size() != word2.size())
-		return false;
-	for (size_t i = 0; i < word1.size(); i++)
-	{
-		if (map.find(word1[i]) != map.end() && map[word1[i]] != word2[i])
+		if (set.count(word[i]) == 0)
 			return false;
-		if (map.find(word2[i]) != map.end() && map[word2[i]] != word1[i])
-			return false;
-		map[word1[i]] = word2[i];
-		map[word2[i]] = word1[i];
 	}
 	return true;
+}
+vector<string> allOneLineStrings(const vector<string>& words)
+{
+	vector<string> result;
+	string line1 = "qwertyuiopQWERTYUIOP";
+	string line2 = "asdfghjklASDFGHJKL";
+	string line3 = "zxcvbnmZXCVBNM";
+	unordered_set<char> set1;
+	unordered_set<char> set2;
+	unordered_set<char> set3;
+	for (size_t i = 0; i < line1.size(); i++)
+	{
+		set1.insert(line1[i]);
+	}
+	for (size_t i = 0; i < line2.size(); i++)
+	{
+		set2.insert(line2[i]);
+	}
+	for (size_t i = 0; i < line3.size(); i++)
+	{
+		set3.insert(line3[i]);
+	}
+	for (size_t i = 0; i < words.size(); i++)
+	{
+		if (containsAll(words[i], set1) || containsAll(words[i], set2) || containsAll(words[i], set3))
+			result.push_back(words[i]);
+	}
+	return result;
+}
+
+//3 zad
+int differentCandies(const vector<int>& candyTypes)
+{
+	size_t candyNumber = candyTypes.size() / 2;
+	unordered_set<int> differentTypes;
+	for (size_t i = 0; i < candyTypes.size(); i++)
+	{
+		if (differentTypes.count(candyTypes[i]) == 0)
+			differentTypes.insert(candyTypes[i]);
+	}
+	return std::min(candyNumber, differentTypes.size());
+}
+
+//4 zad
+bool areAnagram(const string& word1, const string& word2)
+{
+	if (word1.size() != word2.size())
+		return false;
+	unordered_map<char, int> map1, map2;
+	for (size_t i = 0; i < word1.size(); i++)
+	{
+		if (map1.count(word1[i]) == 0)
+			map1.insert({ word1[i], 1 });
+		else
+			++map1[word1[i]];
+	}
+
+	for (size_t i = 0; i < word2.size(); i++)
+	{
+		if (map2.count(word2[i]) == 0)
+			map2.insert({ word2[i], 1 });
+		else
+			++map2[word2[i]];
+	}
+	if (map1.size() != map2.size())
+		return false;
+	for (pair<char, int> elem : map1)
+	{
+		if (map2[elem.first] != elem.second)
+			return false;
+	}
+
+	return true;
+}
+
+vector<vector<string>> anagrams(const vector<string>& words)
+{
+	vector<vector<string>> res;
+	unordered_set<string> anagrams;
+	for (size_t i = 0; i < words.size(); i++)
+	{
+		if (anagrams.count(words[i]) == 0)
+			anagrams.insert(words[i]);
+	}
+	return res;
 }
 
 int main()
 {
 	//1 zad
-	std::vector<std::string> words = { "apple", "banana", "apple", "orange", "banana", "apple" };
-	std::unordered_map<std::string, unsigned> table1 = countWords(words);
-	printWordCount(table1);
+	int X = 10;
+	vector<int> numbers = { 1, 1, 9, 9 };
+	vector<pair<int, int>> result = findAllPairs(numbers, X);
+	for (size_t i = 0; i < result.size(); i++)
+	{
+		std::cout << numbers[result[i].first] << " " << numbers[result[i].second] << std::endl;
+	}
 
 	//2 zad
-	std::vector<std::pair<std::string, std::string>> objects = { {"apple", "fruit"}, {"carrot", "vegetable"}, {"banana", "fruit"}, {"broccoli", "vegetable"} };
-	printCategories(categorize(objects));
+	vector<string> words = { "adsdf", "sfd" };
+	vector<string> result2 = allOneLineStrings(words);
+	for (size_t i = 0; i < result2.size(); i++)
+	{
+		std::cout << result2[i] << std::endl;
+	}
 
 	//3 zad
-	string text = "hello world";
-	vector<pair<char, char>> rules = { {'h', 'H'}, {'o', '0'}, {'e', '3'} };
-	unordered_map<char, char> tableRules = insertRules(rules);
-	changeText(tableRules, text);
-	std::cout << text << std::endl;
+	vector<int> types = { 6, 6, 6, 6 };
+	std::cout << differentCandies(types) << std::endl;
 
 	//4 zad
-	vector<int> numbers = { 8, 7, 2, 5, 3, 1 };
-	int sum = 10;
-	unordered_map<int, size_t> map;
-	for (size_t i = 0; i < numbers.size(); i++)
-	{
-		int complement = sum - numbers[i];
-		if (map.find(complement) != map.end())
-		{
-			std::cout << "(" << map[complement] << ", " << i << ") ";
-		}
-		map[numbers[i]] = i;
-	}
-	std::cout << std::endl;
-
-	//5 zad
-	vector<pair<int, int>> pairs = { {3, 4}, {1, 2}, {5, 2}, {7, 10}, {4, 3}, {2, 5} };
-	findAllSymmetricalPairs(pairs);
-
-	//6 zad
-	string word1 = "ACAB", word2 = "XCXX";
-	string word3 = "ACCB", word4 = "XBBY";
-	std::cout << areIzomorph(word1, word2) << areIzomorph(word3, word4) << std::endl;
-
 	return 0;
 }
